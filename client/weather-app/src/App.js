b537e7cb04e61f5a948f54c9a903fcac0d17cf81
Adyan Reza <adyan.reza@ontariotechu.net>
2022-04-08 15:41:13 -0400

    Front-End Changes
    Added some front end desgins

--- client/weather-app/src/App.js
+++ client/weather-app/src/App.js
@@ -6,9 +6,41 @@ import { Hello } from './components/hello'
 
 function App() {
   return (
-    <div className="App">
-      <Hello />
+    <div className="app">
+      <h2 className='title'>Weather App</h2>
+      <div className="search">
+        <input type="text" placeholder="Enter Location"></input>
+      </div>
 
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Toronto</p>
+          </div>
+          <div className="temp">
+            <h1>15°C</h1>
+          </div>
+          <div className="description">
+            <p>Cloudy with Rain</p>
+          </div>
+        </div>
+
+        <div className="bottom">
+          <div className="feels">
+            <p className='bold'>7°C</p>
+            <p>Feels Like</p>
+          </div>
+          <div className="humidity">
+            <p className='bold'>14%</p>
+            <p>Humidity</p>
+          </div>
+          <div className="wind">
+            <p className='bold'>6 KM/H</p>
+            <p>Wind Speed</p>
+          </div>
+        </div>
+        
+      </div>
     </div>
   );
 }
