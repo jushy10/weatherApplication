61fcd657864697e897d5a90e287a0dac37816cd9
jushy10 <jushy10@yahoo.ca>
2022-04-16 14:07:06 -0400

    Front End Home Page Design Changes

---
+++ client/weather-app/src/components/HourlyForcast.jsx
@@ -0,0 +1,91 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+import { Card, Row, Col } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import "../styles/forecast.css"
+
+class HourlyForcast extends Component {
+constructor() {
+	super();
+	this.state = {
+	};
+}
+
+getDayOfWeek(date) {
+    const dayOfWeek = new Date(date).getDay();
+    return isNaN(dayOfWeek) ? null : 
+      ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'][dayOfWeek];
+}
+
+async forecastAPI() {
+	const options = {
+		method: 'GET',
+		url: 'http://localhost:3001/forecastAPI', //Backend Pull
+		params: { city: this.state.cityName },
+	}
+
+	let data = await axios.request(options)
+	.then(function (response) {
+		return response.data;
+	})
+	.catch(function (error) {
+		console.log(error);
+	});
+	this.setState({ jsonData: data.forecast.forecastday });
+}
+
+handleSubmit = (e) => {
+
+	e.preventDefault();
+	this.forecastAPI();
+
+}
+
+everyChange = (e) => {
+
+	this.setState({ cityName: e.target.value });
+}
+
+
+render() {
+	return (
+		<>
+		<form onSubmit={this.handleSubmit}>
+			<input onChange={this.everyChange} placeholder="Enter Location"></input>
+		</form>
+
+		<div>
+		<br></br><br></br><br></br>	
+		<Row className="card-example d-flex flex-row flex-nowrap overflow-auto">
+		{this.state.jsonData && this.state.jsonData.map(data => {
+			
+			return (
+				<>
+				&emsp;&emsp;
+				<Col>
+				
+				<Card border="success" class="weatherCard" bg='dark' text='light' style={{ width: '25rem', display: 'flex', flexDirection: 'column'}}>
+					<Card.Img class="weatherIcon" variant="top" src={data.day.condition.icon} />
+					<Card.Body>
+						<Card.Title class="cardTitle">{this.getDayOfWeek(data.date)}</Card.Title>
+						<Card.Text class="cardText">Average Temp: {data.day.avgtemp_c}</Card.Text>
+						<Card.Text class="cardText">Chance of Rain: {data.day.daily_chance_of_rain}% </Card.Text>
+
+					</Card.Body>
+				</Card>
+				</Col>
+				</>
+
+			);
+			
+		})}
+		</Row>
+		</div>
+
+	</>
+	);
+}
+}
+
+export default HourlyForcast;
--- client/weather-app/src/components/cityAPI.jsx
+++ client/weather-app/src/components/cityAPI.jsx
@@ -37,13 +37,13 @@ const CityAPI = () => {
 
         axios.request(options).then((response) => {
 
-            setCity(response.data.cityName)
+            setCity(response.data.cityName + ',')
             setTemp(response.data.currentTemp + '°C')
             setCond(response.data.condition)
             setIcon(response.data.icon)
-            setFeelsLike(response.data.feelslike)
-            setHumidity(response.data.humidity)
-            setWind(response.data.wind)
+            setFeelsLike(response.data.feelslike + '°C')
+            setHumidity(response.data.humidity + '%')
+            setWind(response.data.wind + ' km/h')
             setCountry(response.data.countryName)
 
         }).catch((error) => {
@@ -61,7 +61,7 @@ const CityAPI = () => {
                 <div className='container'>
                     <div className='top'>
                         <div className='location'>
-                            <p>{city}, {country}</p>
+                            <p>{city} {country}</p>
                         </div>
                         <div className='temp'>
                             <h1 style={{fontSize: "6rem"}}>{temp}</h1>
@@ -74,20 +74,29 @@ const CityAPI = () => {
                         </div>
                     </div>
 
+                    {feelsLike !== '' && humidity !== '' && wind !== '' &&
                     <div className='bottom'>
                         <div className='feels'>
-                            <p className='bold'>{feelsLike}°C</p>
+                            <p className='bold'>{feelsLike}</p>
+                            {feelsLike !== '' &&
                             <p>Feels Like</p>
+                            }
                         </div>
                         <div className='humidity'>
-                            <p className='bold'>{humidity}%</p>
+                            <p className='bold'>{humidity}</p>
+                            {humidity !== '' &&
                             <p>Humidity</p>
+                            }
                         </div>
                         <div className='wind'>
-                            <p className='bold'>{wind} km/h</p>
+                            <p className='bold'>{wind}</p>
+                            {wind !== '' &&
                             <p>Wind Speed</p>
+                            }
                         </div>
                     </div>
+                    }
+                    
                 </div>
             </div>
         </div>   
